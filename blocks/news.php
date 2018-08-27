205b24b47befa24f73290e59d7f0a2e6519e0ad5
GustavoDGT <gustavo@dgtalliance.com>
2018-08-27 16:23:03 -0500

    medium

--- blocks/news.php
+++ blocks/news.php
@@ -13,17 +13,16 @@ if ( $max_items != 0 ) :
 	foreach ($home_rss_items as $item):
 		$img_url = $item->get_author()->email;	
 ?>      			
-			<li class="grid-25 mobile-grid-100">
+			<li class="grid-25 mobile-grid-100 wip-news">
 	      <div class="thim-widget-icon-box">
 	        <div class="wrapper-box-icon has_custom_image text-center image_box">
 	          <div class="smicon-box iconbox-top">
 	            <div class="boxes-icon">
-	              <img src="<?php echo $img_url; ?>" title="<?php echo $item->get_title(); ?>" alt="<?php echo $item->get_title(); ?>">
+	              <a title="<?php echo $item->get_title(); ?>" href="<?php echo $item->get_link(); ?>"><img src="<?php echo $img_url; ?>" title="<?php echo $item->get_title(); ?>" alt="<?php echo $item->get_title(); ?>"></a>
 	            </div>
 	            <div class="content-inner">
 	              <div class="sc-heading article_heading">
-	                <h4><a href="<?php echo $item->get_link(); ?>" title="<?php echo $item->get_title(); ?>"><?php echo $item->get_title(); ?></a></h4>
-	                <span class="date-publish"><?php echo $item->get_date( 'M' ).' '.$item->get_date( 'd' ).', '.$item->get_date( 'Y' ); ?></span>
+	                <h4><a href="<?php echo $item->get_link(); ?>" title="<?php echo $item->get_title(); ?>"><?php echo $item->get_title(); ?></a><span class="date-publish"><?php echo $item->get_date( 'M' ).' '.$item->get_date( 'd' ).', '.$item->get_date( 'Y' ); ?></span></h4>
 	              </div><div class="desc-icon-box"><div class="desc-content"><?php echo $item->get_content(); ?><br>
 	              <a class="more-new" title="<?php echo $item->get_title(); ?>" href="<?php echo $item->get_link(); ?>">Leer más...</a></div></div>
 	            </div>
