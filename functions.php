51fd33f0bdf0c2753978673a9b2c83fc7ea43cc8
GustavoDGT <gustavo@dgtalliance.com>
2018-08-28 13:32:11 -0500

    no mor style.css

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ function generatepress_child_enqueue_scripts() {
 	$theme_url = get_template_directory_uri();
 	if ( is_rtl() ) {
 		wp_enqueue_style( 'generatepress-rtl', trailingslashit( $theme_url ) . 'rtl.css' );
-		wp_enqueue_style( 'wip-custom', $theme_url . "/style.css", false, WIP_VERSION, 'all' );
 	}	
 }
 add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
