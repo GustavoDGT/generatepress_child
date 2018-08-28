de4e38bee78820377193fb2fb342101fe0de4bb2
GustavoDGT <gustavo@dgtalliance.com>
2018-08-28 15:13:37 -0500

    nosotros

--- content-nosotros.php
+++ content-nosotros.php
@@ -13,10 +13,9 @@ global $post;
 $uploads = wp_upload_dir();
 $upload_path = $uploads['baseurl'];
 ?>
-<section class="grid-100 grid-parent wip-40 skinny-section align-center" id="wip-presentation">
-		<h2>NOSOTROS</h2>
-		<div class="wip-header-line"></div>
-		<div class="grid-40 prefix-5 sufix-5 mobile-grid-100 mobile-prefix-0 mobile-sufix-0">
+<section class="grid-100 grid-parent wip-40 skinny-section" id="wip-presentation">
+  <div class="grid-100 mobile-100 align-justify grid-parent">
+    <div class="grid-40 prefix-5 sufix-5 mobile-grid-100 mobile-prefix-0 mobile-sufix-0">
       <h3>MISIÓN</h3>
       <p>ITSYSTEMS PERU es una organización de profesionales orientados a la transformación digital empresarial que brinda soluciones y servicios relacionados, siendo un activo colaborador y socio estratégico de vanguardia tecnológica.</p>
     </div>
@@ -24,6 +23,7 @@ $upload_path = $uploads['baseurl'];
       <h3>VISIÓN</h3>
       <p>Al 2020 nos posicionaremos como una entidad referente a nivel nacional en educación, poseedores de un gran mecanismo de servicio que forma, produce y difunde conocimiento al desarrollo de la economía digital.</p>
     </div>
+  </div>
 </section>
 <section id="wip-clients" class="grid-100 wip-40 align-center tiny-section">
   <div class="grid-100 mobile-grid-100 wip-20-bottom">
