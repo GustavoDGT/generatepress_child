2e9fdeab43e9e498392321721e700565da4f3b43
GustavoDGT <gustavo@dgtalliance.com>
2018-09-20 17:17:33 -0500

    suscription form add

--- blocks/modal-suscription.php
+++ blocks/modal-suscription.php
@@ -7,13 +7,13 @@ $upload_path = $uploads['baseurl'];
 ?>
 <div id="wip-modal-suscription" class="overlay_modal">
   <button class="close close-modal" data-id="wip-modal-suscription">X</button>
-  <div class="modal_cm grid-100 mobile-grid-100 grid-parent wip-40 align-center bg-section" style="background-image: url( '<?php echo $upload_path . '/2018/09/bg-suscription.jpg'; ?>' )">
+  <div class="modal_cm grid-100 mobile-grid-100 grid-parent wip-40 align-center bg-section" style="background-image: url( '<?php echo $upload_path . '/2018/09/bg_suscription.jpg'; ?>' )">
     <div class="grid-100 mobile-grid-100 grid-parent">
       <h2 class="wip-title frase"><span class="wip-title-init">Descargar pack</span> <span class="wip-title-final">para aprender más sobre SAP</span></h2>
     </div>
     <div class="wip-form wip-40-top grid-100 mobile-grid-100 grid-parent small-section">
       <div class="content">
-        <?php echo do_shortcode( '[contact-form-7 id="1067" title="Suscription Form"]' ); ?>
+        <?php echo do_shortcode( '[contact-form-7 title="Suscription Form"]' ); ?>
       </div>
     </div>
   </div>
