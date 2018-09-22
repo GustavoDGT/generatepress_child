c294fc7f7d333dfca8e710ad846d6f1e11d1bb67
GustavoDGT <gustavo@dgtalliance.com>
2018-09-21 19:20:38 -0500

    d

--- blocks/modal-suscription.php
+++ blocks/modal-suscription.php
@@ -7,7 +7,7 @@ $upload_path = $uploads['baseurl'];
 ?>
 <div id="wip-modal-suscription" class="overlay_modal">
   <button class="close close-modal" data-id="wip-modal-suscription">X</button>
-  <div class="modal_cm grid-100 mobile-grid-100 grid-parent wip-40 align-center bg-section" style="background-image: url( '<?php echo $upload_path . '/2018/09/bg_suscription.png'; ?>' )">
+  <div class="modal_cm grid-100 mobile-grid-100 grid-parent wip-40 align-center bg-section" style="background-image: url( '<?php echo $upload_path . '/2018/09/bg_final_suscription.png'; ?>' )">
     <div class="grid-100 mobile-grid-100 grid-parent">
       <h2 class="wip-title frase"><span class="wip-title-init">Descargar pack</span> <span class="wip-title-final">para aprender más sobre SAP</span></h2>
     </div>
