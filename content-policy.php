8925c53a02e79d988bf3fdf7cc5e05032335e2f0
GustavoDGT <gustavo@dgtalliance.com>
2018-10-19 16:39:59 -0500

    pez

---
+++ content-policy.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template used for displaying page content in page-inhouse.php
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit; // Exit if accessed directly.
+}
+?>
+<section class="grid-100 grid-parent wip-40 skinny-section" id="wip-presentation">
+  <div class="grid-100 mobile-100 align-center grid-parent">
+    <p>El USUARIO acepta que los datos personales aportados en el momento de su registro, o cualquier otro facilitado a ITSystems Perú para el acceso a sus servicios web, sean utilizados con la finalidad de facilitar la prestación de los servicios solicitados, para la correcta identificación de los usuarios que solicitan servicios personalizados, para la realización de estudios estadísticos de los usuarios que permitan diseñar mejoras en los servicios prestados, para la gestión de tareas básicas de administración, así como para mantenerle informado, bien por correo electrónico o bien por cualquier otro medio de novedades, productos y servicios relacionados con ITSystems Perú. Sus datos se mantendrán en privado y no serán expuestos al público o a otras entidades.</p>
+  </div>
+</section>
